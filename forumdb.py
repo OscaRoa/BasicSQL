787b5616db1f3de88380b48b02c00b7ee1a22797
Oscar Roa <oshardy@gmail.com>
2015-09-30 22:47:57 +0000

    .gitignore added and fix SQL injection bug in forumdb.py

--- forumdb.py
+++ forumdb.py
@@ -38,7 +38,7 @@ def AddPost(content):
     DB = psycopg2.connect("dbname=forum")
     c = DB.cursor()
     c.execute(
-        "INSERT INTO posts (content) VALUES ('%s')" % content
+        "INSERT INTO posts (content) VALUES (%s)", (content,) 
     )
     DB.commit()
     DB.close()
